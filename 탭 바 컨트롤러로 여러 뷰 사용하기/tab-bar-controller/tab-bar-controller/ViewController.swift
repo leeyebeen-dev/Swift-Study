d8ce9c8bf0491d67576114a596670301f3ae0739
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-08-18 11:11:58 +0900

    Build : 탭바 컨트롤러 창 만들기

---
+++ 탭 바 컨트롤러로 여러 뷰 사용하기/tab-bar-controller/tab-bar-controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tab-bar-controller
+//
+//  Created by 이예빈 on 2023/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 탭 바 컨트롤러로 여러 뷰 사용하기/tab-bar-controller/tab-bar-controller/ViewController2.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController2.swift
+//  tab-bar-controller
+//
+//  Created by 이예빈 on 2023/08/18.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
